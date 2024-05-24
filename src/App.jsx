a1b96002e7c13a7ac3911915d6aed568972bf18e
Raymond <njagala.raymond@gmail.com>
2024-05-24 05:15:18 +0300

    added components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import Header from "./components/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+ import React from 'react';
 
 
 function App() {
   
   return (
     <>
-       
+       <Header/>
+
+ 
+  
+
+
     </>
   )
 }
