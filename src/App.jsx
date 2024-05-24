5c61b7b38c689890a2c5b46842389b5ce23348eb
Raymond <njagala.raymond@gmail.com>
2024-05-24 06:18:44 +0300

    added routes in the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,39 @@
-import Header from "./components/Header";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
  import React from 'react';
-
+ import Home from "./components/Home";
+ import About from "./components/About";
+import Appointment from "./components/Appointment";
+import Contact from "./components/Contact";
+import Blog from "./components/Blog";
+import Services from "./components/Services";
 
 function App() {
   
   return (
     <>
+    <BrowserRouter>
+    
        <Header/>
+
+       <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/services" element={<Services/>} />
+        <Route path="/blog" element={<Blog/>} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path="/appointment" element={<Appointment/>} />
+        
+
+        
+
+        
+
+       </Routes>
+
+
+
+    </BrowserRouter>
 
  
   
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,37 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Header (){
   return (
-    <div>Header
-        
+    <div>
+        <ul>
+            <li className=''>
+                <Link to="/">The AgriHub</Link>
+            </li>
+            <li className=''>
+                <Link to="/">Home</Link>
+            </li>
+            <li className=''>
+                <Link to="/about">About</Link>
+            </li>
+            <li className=''>
+                <Link to="/services">Services</Link>
+            </li>
+
+            <li className=''>
+                <Link to="/blog">Blog</Link>
+            </li>
+
+            <li className=''>
+                <Link to="/contact">Contact</Link>
+            </li>
+            <li className=''>
+                <Link to="/appointment">Appointment</Link>
+            </li>
+        </ul>
+
     </div>
   )
 }
 
-export default Header
+export default Header;
