8278f12fefc5bb919fc26f02a8ba9a0400cd3160
Raymond <njagala.raymond@gmail.com>
2024-06-13 02:12:32 +0300

    changed colors in the nav bar

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 function Contact () {
   return (
-    <div>Contact
+    <div>
+      <h1 className='mt-8 text-center text-2xl font-bold text-lime-700'>GET IN TOUCH</h1>
+      <h1>GOT A QUESTION</h1>
+      <h1>CONTACT AGRIHUB</h1>
+      <p>We'd Love to Hear From You</p>
+      <p>We're here to help and answer any question you might have. Whether you're curious about features, a free trial, or even press-we're ready to answer any and all questions. We look forward to hearing from you</p>
     </div>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,29 +5,29 @@ function Header (){
   return (
     <div className='header'>
         <ul>
-            <li className='text-black bg-white font-extrabold h-7'>
+            <li className='text-black bg-white font-extrabold h-7 hover:text-[#ADD8E6]'>
                 <Link to="/">The AgriHub</Link>
             </li>
             
-            <li className='text-white font-extrabold hover:border-b-4 hover:text-[#33691E]'>
+            <li className='text-white font-extrabold hover:border-b-4 hover:text-[#ADD8E6]'>
                 <Link to="/">Home</Link>
             </li>
-            
-            <li className='text-white font-extrabold hover:border-b-4 hover:text-[#33691E]'>
+            {/* #33691E */}
+            <li className='text-white font-extrabold hover:border-b-4 hover:text-[#ADD8E6]'>
                 <Link to="/about">About</Link>
             </li>
-            <li className='text-white font-extrabold hover:border-b-4 hover:text-[#33691E]'>
+            <li className='text-white font-extrabold hover:border-b-4 hover:text-[#ADD8E6]'>
                 <Link to="/services">Services</Link>
             </li>
 
-            <li className='text-white font-extrabold hover:border-b-4 hover:text-[#33691E]'>
+            <li className='text-white font-extrabold hover:border-b-4 hover:text-[#ADD8E6]'>
                 <Link to="/blog">Blog</Link>
             </li>
 
-            <li className='text-white font-extrabold hover:border-b-4 hover:text-[#33691E]'>
+            <li className='text-white font-extrabold hover:border-b-4 hover:text-[#ADD8E6]'>
                 <Link to="/contact">Get in Touch</Link>
             </li>
-            <li className='text-white font-extrabold hover:border-b-4 hover:text-[#33691E]'>
+            <li className='text-white font-extrabold hover:border-b-4 hover:text-[#ADD8E6]'>
                 <Link to="/appointment">Appointment</Link>
             </li>
         </ul>
