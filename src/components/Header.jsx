9880b819f6c85e796fd29fb917bec06853bf42a6
Raymond <njagala.raymond@gmail.com>
2024-06-12 23:26:13 +0300

    Added styles to the document

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,29 +3,31 @@ import { Link } from 'react-router-dom'
 
 function Header (){
   return (
-    <div>
+    <div className='header'>
         <ul>
-            <li className=''>
+            <li className='text-black bg-white font-extrabold h-7'>
                 <Link to="/">The AgriHub</Link>
             </li>
-            <li className=''>
+            
+            <li className='text-white font-extrabold hover:border-b-4 hover:text-[#33691E]'>
                 <Link to="/">Home</Link>
             </li>
-            <li className=''>
+            
+            <li className='text-white font-extrabold hover:border-b-4 hover:text-[#33691E]'>
                 <Link to="/about">About</Link>
             </li>
-            <li className=''>
+            <li className='text-white font-extrabold hover:border-b-4 hover:text-[#33691E]'>
                 <Link to="/services">Services</Link>
             </li>
 
-            <li className=''>
+            <li className='text-white font-extrabold hover:border-b-4 hover:text-[#33691E]'>
                 <Link to="/blog">Blog</Link>
             </li>
 
-            <li className=''>
-                <Link to="/contact">Contact</Link>
+            <li className='text-white font-extrabold hover:border-b-4 hover:text-[#33691E]'>
+                <Link to="/contact">Get in Touch</Link>
             </li>
-            <li className=''>
+            <li className='text-white font-extrabold hover:border-b-4 hover:text-[#33691E]'>
                 <Link to="/appointment">Appointment</Link>
             </li>
         </ul>
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 function Services (){
   return (
-    <div>Services
+    <div>
+      Services
     </div>
   )
 }
