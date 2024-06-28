22bb8f2c0abec03e167f01749a9a66a549eb44bb
Raymond <njagala.raymond@gmail.com>
2024-06-28 10:58:20 +0300

    added content to the appointments page

--- src/components/Appointment.jsx
+++ src/components/Appointment.jsx
@@ -1,9 +1,66 @@
 import React from 'react'
+import { MantineProvider } from '@mantine/core';
+import { TextInput, rem } from '@mantine/core';
+import { IconAt } from '@tabler/icons-react';
+import { Button } from '@mantine/core';
+
 
 function Appointment() {
+  const icon = <IconAt style={{ width: rem(16), height: rem(16) }} />;
   return (
-    <div>Appointment
+    <div className='bg-[#1E3D3B]'>
+      <div className='flex gap-8 pt-4 ml-4'>
+        <img src="https://images.pexels.com/photos/5685756/pexels-photo-5685756.jpeg?auto=compress&cs=tinysrgb&w=800" className='rounded-2xl'/>
+        <div className='flex flex-col'>
+          <h1 className='appoint pt-16 text-center text-4xl pb-6'>APPOINTMENTS</h1>
+          <p className='appoint-1 text-2xl mx-auto max-w-[80%]'>Feel free to reach out to us and schedule an appointment with our experts whether you want to be educated about the best farming practices, ask about diseases and pests that are giving you hard time or anything, feel free to book an appointment below</p>
+        </div>
+      </div>
+      <MantineProvider>
 
+      <div className='text mx-auto max-w-[30%] pt-20'>
+        <TextInput
+        label="First Name"
+        size="md"
+        radius="md"
+        placeholder="Please enter your First Name"
+        w={400}
+        className='pb-4'
+        />
+        <TextInput
+        label="Last Name"
+        size="md"
+        placeholder="Please enter your Last Name"
+        w={400}
+        radius="md"
+        className='pb-4'
+        />
+        <TextInput
+        label="Phone Number"
+        size="md"
+        placeholder="Please enter your Phone Number"
+        w={400}
+        radius="md"
+        className='pb-4'
+        />
+        <TextInput
+          leftSectionPointerEvents="none"
+          leftSection={icon}
+          size="md"
+          label="Your email"
+          placeholder="Please enter your Email"
+          w={400}
+          radius="md"
+          className='pb-4'
+        />
+        <div className='mx-auto ml-24'>
+          <Button variant="filled" color="green" size="md" radius="lg" className='mb-8 '>Book an Appointment
+          </Button>
+        </div>
+      </div>
+
+      
+      </MantineProvider>
     </div>
   )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,16 +5,20 @@ import Underline from './Underline'
 import Privacy from './Privacy'
 
 function Footer ()  {
+  
+  
+  
   return (
-    <div className='bg-[#8FBC8F] pt-12 pb-12'>
-      <div className='flex gap-12 mx-auto max-w-[60%] footer'>
+    <div className='bg-[#8FBC8F] pt-12 pb-12 '>
+      <div className='flex gap-12 mx-auto max-w-[60%]  footer'>
         <div className='flex flex-col'>
-          <h1 className='mb-4'><Link to="/" className='underline-effect'><PointingArrow/>The AgriHub</Link></h1>
-          {/* <p>Empowering Agriculture, One Connection at a Time</p> */}
+          <h1 className='mb-4'><Link to="/" onClick={(e) => handleClick(e, 'home')}className='underline-effect'><PointingArrow/>The AgriHub</Link></h1>
+
           <div className='links flex flex-col '>
-            <Link to="/about" className='underline-effect'><PointingArrow/>About us</Link>
-            <Link to="/contact" className='underline-effect'><PointingArrow/>Get in Touch</Link>
-            <Link to="/appointment" className='underline-effect'><PointingArrow/>Appointment</Link>
+            <Link to="/about" onClick={(e) => handleClick(e, 'about')} className='underline-effect'><PointingArrow/>About us</Link>
+            <Link to="/contact" onClick={(e) => handleClick(e, 'contact')}className='underline-effect'><PointingArrow/>Get in Touch</Link>
+            <Link to="/appointment" onClick={(e) => handleClick(e, 'appointment')}className='underline-effect'><PointingArrow/>Appointment</Link>
+            
              <Link to="/privacy" className='underline-effect'><PointingArrow/>Privacy</Link>
             
           </div>
