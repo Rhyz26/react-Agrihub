12a118d3fb432ef4298a18a653ca4f1e8274c2f8
Raymond <njagala.raymond@gmail.com>
2024-09-24 20:56:11 +0300

    worked on card that appears when you click on it

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -95,9 +95,9 @@ function About () {
     <div className='flex flex-col items-center border-2 border-[#8B4513] w-64 h-96 pt-2 rounded-lg hover:border-blue-600 transition duration-300 ease-in-out shadow-md hover:shadow-xl'>
       <img src="/ray11.jpg" width={80} className='rounded-full border-2 border-[#8B4513] mb-4 '/>
       <h1>Raymond Njagala</h1>
-      <p className='pb-8'>CEO</p>
+      <p className='pb-12'>CEO</p>
       
-      <button className='pb-12 text-gray-800'><Persona/></button>
+      <button className='pb-20 text-gray-800'><Persona/></button>
       
 
       <a href='https://www.linkedin.com/in/raymond-njagala-9a6309a8' target="_blank"
@@ -111,8 +111,8 @@ function About () {
     <div className='flex flex-col items-center border-2 border-[#8B4513] w-64 h-96 pt-2 rounded-lg'>
       <img src="/ray11.jpg" width={80} className='rounded-full border-2 border-[#8B4513] mb-4'/>
       <h1>Grace Kyomuhendo</h1>
-      <p className='pb-8'>Lead Agronomist</p>
-      <button className='pb-4 text-gray-800'><Persona/></button>
+      <p className='pb-12'>Lead Agronomist</p>
+      <button className='pb-20 text-gray-800'><Persona/></button>
       
       <a href='https://www.linkedin.com/in/raymond-njagala-9a6309a8' target="_blank"
 
@@ -122,8 +122,8 @@ function About () {
     <div className='flex flex-col items-center border-2 w-64 h-96 border-[#8B4513] pt-2 rounded-lg'>
       <img src="/ray11.jpg" width={80} className='rounded-full border-2 border-[#8B4513] mb-4'/>
       <h1>David Tumusiime</h1>
-      <p className='pb-8'>Soil Scientist</p>
-      <button className='pb-4 text-gray-800'><Persona/></button>
+      <p className='pb-12'>Soil Scientist</p>
+      <button className='pb-20 text-gray-800'><Persona/></button>
       
       <a href='https://www.linkedin.com/in/raymond-njagala-9a6309a8' target="_blank"
 
@@ -134,7 +134,7 @@ function About () {
 <div className='flex flex-col items-center border-2 border-[#8B4513] w-64 h-96 pt-2 rounded-lg'>
   <img src="/ray11.jpg" width={80} className='rounded-full border-2 border-[#8B4513] mb-4'/>
         <h1 className=''>Sarah Nakyobe</h1>
-      <p className='text-center pb-3'>Agri-Marketing Specialist</p>
+      <p className='text-center pb-6'>Agri-Marketing Specialist</p>
       <button className='pb-4 text-gray-800'><Persona/></button>
       <a href='https://www.linkedin.com/in/raymond-njagala-9a6309a8' target="_blank"
 
